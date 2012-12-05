c2db7d3ef5f56b8df7d0a94848b36f8e1e36fd72
Richec <roushenzo@gmail.com>
2012-12-05 15:15:16 +0300

    add project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Muscoll::Application.config.secret_token = 'a5fe25a79872bcfc963a6325ac0acf832536767ade11a9a615c32f4064830fedfd0f45f341e3b6d1754bf8f6662b5a212d189eb8fda8f1dd4860c32af6612161'
